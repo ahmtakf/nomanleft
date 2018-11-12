32f470fe525a2db21dd738d96a74a3fcd38a9f3a
Ahmet Akif Uğurtan <akif.ugurtan@gmail.com>
2018-11-12 21:14:26 +0300

    project structure ...

---
+++ src/nomanleft/src/com/nullpointers/nomanleft/Main.java
@@ -0,0 +1,12 @@
+package main.com.nullpointers.nomanleft;
+
+import main.com.nullpointers.nomanleft.controller.GameManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameManager gm = new GameManager();
+        gm.startLevel(1);
+    }
+
+}
---
+++ src/nomanleft/src/com/nullpointers/nomanleft/controller/GameManager.java
@@ -0,0 +1,21 @@
+package main.com.nullpointers.nomanleft.controller;
+
+import main.com.nullpointers.nomanleft.controller.FileManager;
+import main.img.nullpointers.nomanleft.view.LevelPanel;
+
+import javax.swing.*;
+
+public class GameManager {
+
+    public GameManager(){
+        System.out.println(FileManager.getInstance().getCustomization());
+    }
+
+    public void startLevel(int levelNumber){
+        JFrame frame = new JFrame("LevelPanel");
+        frame.setContentPane(new LevelPanel().LevelPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
