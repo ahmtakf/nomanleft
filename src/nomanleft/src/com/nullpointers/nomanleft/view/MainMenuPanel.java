fba9bb216b588fc57660db62ba976894ebcaace7
berkay <brkykrlk@gmail.com>
2018-11-12 22:43:08 +0300

    mainMenuPanel added.

--- src/nomanleft/src/com/nullpointers/nomanleft/view/MainMenuPanel.java
+++ src/nomanleft/src/com/nullpointers/nomanleft/view/MainMenuPanel.java
@@ -1,6 +1,8 @@
-package nomanleft.src.com.nullpointers.nomanleft;
+package com.nullpointers.nomanleft.view;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.* ;
 import javax.swing.border.EmptyBorder;
 
@@ -22,28 +24,51 @@ public class MainMenuPanel extends JPanel{
     public MainMenuPanel(){
         super();
         setBorder(new EmptyBorder(20, 20, 20, 20));
-        this.setPreferredSize(new Dimension(500,500));
+        this.setPreferredSize(new Dimension(1600 ,900));
 
-    //initialize buttons
+    //initialize buttons, set action commands, add boarders
 
         EmptyBorder buttonBorder =  new EmptyBorder(10, 20, 10, 20);
+
         playGame = new JButton("PlayGame");
         playGame.setBorder(buttonBorder);
+        playGame.setActionCommand("PlayGame");
+        playGame.addActionListener(new ButtonListener());
+
         timeTrial = new JButton("TimeTrial");
         timeTrial.setBorder(buttonBorder);
-        sandbox = new JButton("Sandbox");
+        timeTrial.setActionCommand("TimeTrial");
+        timeTrial.addActionListener(new ButtonListener());
+
+        sandbox = new JButton("SandBox");
         sandbox.setBorder(buttonBorder);
+        sandbox.setActionCommand("SandBox");
+        sandbox.addActionListener(new ButtonListener());
+
         shop = new JButton("Shop");
         shop.setBorder(buttonBorder);
+        shop.setActionCommand("Shop");
+        shop.addActionListener(new ButtonListener());
+
         howToPlay = new JButton("HowToPlay");
         howToPlay.setBorder(buttonBorder);
+        howToPlay.setActionCommand("HowToPlay");
+        howToPlay.addActionListener(new ButtonListener());
+
         options = new JButton ("Options");
         options.setBorder(buttonBorder);
+        options.setActionCommand("Options");
+        options.addActionListener(new ButtonListener());
+
         credits = new JButton ("Credits");
         credits.setBorder(buttonBorder);
+        credits.setActionCommand("Credits");
+        credits.addActionListener(new ButtonListener());
+
         exit = new JButton("Exit");
         exit.setBorder(buttonBorder);
-
+        exit.setActionCommand("Exit");
+        exit.addActionListener(new ButtonListener());
 
     //initialize Label
         gameName = new JLabel ("<html><h1><strong><i>NoManLeft</i></strong></h1><hr></html>");
@@ -86,34 +111,56 @@ public class MainMenuPanel extends JPanel{
     }
 
     public void playGame() {
-
+        System.out.println("PlayGame");
     }
 
     public void openTimeTrial() {
-
+        System.out.println("openTimeTrial");
     }
 
     public void openSandBox() {
-
+        System.out.println("openSandBox");
     }
 
     public void openShop() {
-
+        System.out.println("openShop");
     }
 
     public void openHowToPlay(){
-
+        System.out.println("openHowToPlay");
     }
 
     public void openCredits(){
-
+        System.out.println("openCredits");
     }
 
     public void openOptions(){
-
+        System.out.println("openOptions");
     }
 
     public void quitGame(){
+        System.out.println("quitGame");
+    }
+
+
+    //implement listeners
+
+    private class ButtonListener implements ActionListener {
+
+        public void actionPerformed (ActionEvent event) {
+             String actionCommand = event.getActionCommand();
+            switch (actionCommand) {
+                case "PlayGame" :   playGame(); break;
+                case "TimeTrial":   openTimeTrial() ; break ;
+                case "SandBox":     openSandBox();  break ;
+                case "Shop":        openShop(); break ;
+                case "HowToPlay":   openHowToPlay(); break ;
+                case "Credits":     openCredits(); break ;
+                case "Options":     openOptions(); break;
+                case "Exit":        quitGame(); break ;
+
+            }
+        }
 
     }
 
