21d139e3546350835256e5fc3c922dc47dc16d8a
Ahmet Akif Uğurtan <akif.ugurtan@gmail.com>
2018-11-17 17:47:51 +0300

    Factory bug fix

--- src/nomanleft/src/com/nullpointers/nomanleft/Model/MapObjectFactory.java
+++ src/nomanleft/src/com/nullpointers/nomanleft/Model/MapObjectFactory.java
@@ -1,11 +1,11 @@
 package com.nullpointers.nomanleft.model;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MapObjectFactory {
 
-    private static  HashMap<Human,Tile> hm = new HashMap<Human,Tile>();
-
+    private static  HashMap<String, MapObject> hm = new HashMap<>();
 
     public MapObject getTile(Tile tile){
         MapObject tiles = null;
@@ -20,22 +20,29 @@ public class MapObjectFactory {
                 case "Tower":
                     tiles = new Tower(tile.getPoint());
                     System.out.println("Tower is created\n");
+                    break;
                 case "Bush":
                     tiles = new Bush(tile.getPoint());
                     System.out.println("Bush is created\n");
+                    break;
                 case "Ground":
                     tiles = new Ground(tile.getPoint());
                     System.out.println("Ground is created\n");
+                    break;
                 case "WallTile":
                     tiles = new WallTile(tile.getPoint());
                     System.out.println("WallTile is created\n");
+                    break;
                 case "Mountain":
                     tiles = new Mountain(tile.getPoint());
                     System.out.println("Mountain is created\n");
+                    break;
                 case "Lava":
                     tiles = new Lava(tile.getPoint());
                     System.out.println("Lava is created\n");
+                    break;
             }
+            hm.put(type, tile);
         }
 
         return tiles;
@@ -53,13 +60,17 @@ public class MapObjectFactory {
                 case "BattleRam":
                     humans = new BattleRam(human.getPoint());
                     System.out.println("BattleRam is created\n");
+                    break;
                 case "Soldier":
                     humans = new Soldier(human.getPoint());
                     System.out.println("Soldier is created\n");
+                    break;
                 case "Peasent":
                     humans = new Peasent(human.getPoint());
                     System.out.println("Peasent is created\n");
+                    break;
             }
+            hm.put(type, humans);
         }
         return humans;
     }
